38fb8eed2a69f2cdb7dad1cf93ae01155915d7b2
Alex Forsythe <awforsythe@gmail.com>
2015-07-12 23:55:54 -0700

    Added recursive subdivision to triangle.

--- webgl/sandbox/sandbox.js
+++ webgl/sandbox/sandbox.js
@@ -1,45 +1,68 @@
 "use strict";
 
+var canvas;
 var gl;
-var points;
+
+var points = [];
 
 window.onload = function init()
 {
-    var canvas = document.getElementById( "gl-canvas" );
+    var canvas = document.getElementById("gl-canvas");
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
 
+    var vertices = [
+        vec2(-1.0, -1.0),
+        vec2( 0.0,  1.0),
+        vec2( 1.0, -1.0)
+    ];
 
-    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
+    divideTriangle(vertices[0], vertices[1], vertices[2], 1);
 
-    //  Configure WebGL
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
-    //  Load shaders and initialize attribute buffers
-
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-
-    // Load the data into the GPU
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
     var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
-    // Associate out shader variables with our data buffer
-
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
 
     render();
 };
 
+function triangle(a, b, c)
+{
+    points.push(a, b, c);
+}
 
-function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+function divideTriangle(a, b, c, count)
+{
+    if (count === 0)
+    {
+        triangle(a, b, c);
+    }
+    else
+    {
+        var ab = mix(a, b, 0.5);
+        var ac = mix(a, c, 0.5);
+        var bc = mix(b, c, 0.5);
+
+        divideTriangle(a, ab, ac, count - 1);
+        divideTriangle(b, bc, ab, count - 1);        
+        divideTriangle(c, ac, bc, count - 1);
+        divideTriangle(ab, ac, bc, count - 1);
+    }
+}
+
+function render()
+{
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length);
 }
